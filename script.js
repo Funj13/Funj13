ed2ad237d6741e30e04f787ed4cc4108588f81e0
Caique Pires <108283643+Funj13@users.noreply.github.com>
2022-06-27 18:30:33 -0300

    Death Impact
    yaa

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelector('.input');
+
+inputs.forEach(input) => input.addEventListener('focus', handlefocus) {
+    
+};
+
+inputs[0].addEventListener('click',console.log('testando'))
+
+inputs[1].addEventListener('click',console.log('testando'))
+
